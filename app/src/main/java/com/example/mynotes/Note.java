171e15d3ad193e38a19cc62466a703c25d0e03d1
U170709017 <aynursalman@posta.mu.edu.tr>
2020-12-17 15:48:23 +0300

    Initial commit

---
+++ app/src/main/java/com/example/mynotes/Note.java
@@ -0,0 +1,40 @@
+package com.example.mynotes;
+
+import java.util.Date;
+
+public class Note {
+
+    private String header;
+    private Date date;
+    private String filePath;
+
+    public Note(String header, Date date, String filePath) {
+        this.header = header;
+        this.date = date;
+        this.filePath = filePath;
+    }
+
+    public String getHeader() {
+        return header;
+    }
+
+    public void setHeader(String header) {
+        this.header = header;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getFilePath() {
+        return filePath;
+    }
+
+    public void setFilePath(String filePath) {
+        this.filePath = filePath;
+    }
+}
